3180a8323685472417b2b890c6967b9a068f57fa
madacgrav <madacgrav@gmail.com>
2016-03-12 20:37:27 -0600

    new project

---
+++ MVCWeb/CarMaint/CarMaint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarMaint.Startup))]
+namespace CarMaint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
